5ef48020e1ce6806a4a9a35c977779814460a228
devyanshgoyal <56020890+devyanshgoyal@users.noreply.github.com>
2019-10-16 17:01:54 +0530

    loggerbysharad

---
+++ LogUI.java
@@ -0,0 +1,14 @@
+package com.cg.jdbc.ui;
+
+import org.apache.log4j.Logger;
+
+public class LogUI {
+	static Logger log = Logger.getLogger(LogUI.class.getName());
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		log.info("Hello info");
+				
+		
+	}
+
+}
